f02bebb5d9ead5d239b41cd64033d5d1ed194b98
ervinbani <bani.ervin@gmail.com>
2018-04-04 21:02:11 +0200

    final

--- main.js
+++ main.js
@@ -12,24 +12,37 @@ $(document).ready(function() {
   console.log(arraybomba);
 
 
-
+  var bombe=0;
+  var vuote=0;
 
   $(".colonna").click(function(){
-    console.log($(this).html());
-      
-    var control=arraybomba.includes(parseInt($(this).html()));
+    console.log($(this).attr("id"));
+
+    var control=arraybomba.includes(parseInt($(this).attr("id")));
 
 
 
     if(control==true){
 
      $(this).css('background', 'red');
+     bombe=bombe+1;
+     $('#numbombe').html(bombe);
+
    }
      else   {
        $(this).css('background', 'green');
+       voute=vuote+1;
+       $('#casellevuote').html(vuote);
+
      }
 
   });
+  $('#numbombe').css('background-color', 'red');
+
+  $('#casellevuote').css('background-color', 'green');
+
+
+
 
   function random(){
     var randomnumber=Math.floor(Math.random()*64+1);
